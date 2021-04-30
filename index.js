4ab0806ba5195ec40c623100dfd2ab28a01f6d27
Niels <nkoster@users.noreply.github.com>
2021-04-30 17:17:12 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+(_ => {
+
+  const express = require('express')
+  const app = express()
+  const cors = require('cors')
+
+  const apiPort = process.env.APIPORT || 3389
+
+  app.use(cors())
+  app.use(express.json())
+  
+  const api = async (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    res.send(JSON.stringify(req.body))
+  }
+
+  app.post('/api/v1/kafka', api)
+
+  app.listen(apiPort, _ => console.log('Listening at port', apiPort))
+
+})()
